f13bda0dab0b5d9330ab2954489838b28c30f562
Anđela <andjelamiskovic2000@gmail.com>
2023-03-06 22:16:01 +0100

    login page design

---
+++ src/frontend/src/components/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div style="display: flex; margin-top: 25px; margin-left: 30px">
+    <img src="../assets/logo.png" style="height: 35px" alt="logo">
+  </div>
+  <div style="display: flex; justify-content: center; align-items: center; height: 80%; position: relative">
+    <div>
+      <h2>Login</h2>
+      <p>Welcome back to Nutrical!</p>
+      <span class="p-input-icon-left" style="margin-bottom: 10px; width: 100%">
+            <i class="pi pi-user"/>
+            <InputText type="text" v-model="email" placeholder="Email"/>
+      </span>
+      <br>
+      <span class="p-input-icon-left" style="margin-bottom: 10px; width: 100%">
+            <i class="pi pi-lock"/>
+            <InputText type="text" v-model="password" placeholder="Password"/>
+      </span>
+      <br>
+      <Button style="width: 100%; text-align: center">Login</Button>
+    </div>
+  </div>
+  <div>
+    <p style="width: 100%; float: right; margin-right: 10px">
+      Don't have an account?
+      <span><b>Create one</b></span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: ""
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/frontend/src/pages/HomePage.vue
+++ src/frontend/src/pages/HomePage.vue
@@ -1,34 +1,100 @@
 <template>
-  <body>
-  <div class="container">
-    <div class="signin-signup">
-      <form action="" class="sign-in-form">
-        <h2 class="title">Sign in</h2>
-        <div class="input-field">
-          <i class="fas fa-user"></i>
-          <input type="text" placeholder="Username">
+  <body class="homepage-body">
+    <Card class="loginCard">
+      <template #content>
+        <div class="row">
+          <div class="col-5">
+            <Login></Login>
+          </div>
+          <div class="col-7">
+            <img alt="" src="../assets/images/recipes.jpg" style="height: 100%; object-fit: cover">
+          </div>
         </div>
-        <div class="input-field">
-          <i class="fas fa-lock"></i>
-          <input type="password" placeholder="Password">
-        </div>
-        <Button
-            class="p-button-rounded p-button-primary"
-        >Login
-        </Button>
-      </form>
-    </div>
-    <div class="panels-container"></div>
-  </div>
-
+      </template>
+    </Card>
   </body>
 </template>
+
 <script>
+import Login from "@/components/Login";
+
 export default {
   name: "HomePage",
-  components: {}
-}
+  components: {Login},
+  data() {
+    return {
+      loginActive: true,
+    };
+  },
+  methods: {
+    toggle() {
+      this.loginActive = !this.loginActive;
+    },
+  },
+};
 </script>
-<style>
 
+<style>
+h2 {
+  font-size: 2.8rem !important;
+  font-weight: 700;
+}
+
+.homepage-body {
+  background-color: var(--white);
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+}
+
+.p-card .p-card-body,
+.p-card .p-card-body .p-card-content {
+  padding: 0;
+  position: relative;
+  height: 100%;
+}
+
+.row {
+  position: relative;
+  height: 100%
+}
+
+a,
+button {
+  cursor: pointer;
+}
+
+.feature img {
+  width: 100%;
+  max-width: 480px;
+}
+
+@media (max-width: 575px) {
+  #banner h1 {
+    font-size: 3.6rem;
+  }
+}
+
+input {
+  width: 100%;
+  padding: 5px;
+  border-radius: 4px;
+  margin: 5px 0;
+  display: inline-block;
+  font-size: 12px;
+}
+
+.loginCard {
+  background: #fff;
+  overflow: hidden;
+  border-radius: 5px;
+  box-shadow: 1px 1px 2px gray;
+  height: 70%;
+  width: 80%;
+  position: absolute;
+}
 </style>
